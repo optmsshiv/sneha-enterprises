1160d8a7ca9915ca58526e8dca682a716b7546e7
optmsshiv <optmsshiv@gmail.com>
2026-05-16 17:19:22 +0530

    error log added

--- api/index.php
+++ api/index.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 // ============================================================
 //  SNEHA ENTERPRISES — SINGLE FILE API ROUTER v2.1
 //  ALL requests handled here — NO .htaccess needed
